82e9ab2ce879359f3781012d42a9fcaf1e10a168
hankka86 <hanna.liebrecht@gmail.com>
2020-08-02 11:27:40 +0200

    brakujace pliki

---
+++ src/test/java/Przykłady/Przyklad2Test.java
@@ -0,0 +1,45 @@
+package Przykłady;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Przyklad2Test {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/chromedriver");
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        driver = new ChromeDriver();
+
+        // Zmaksymalizuj okno przeglądarki
+        driver.manage().window().maximize();
+        // Przejdź do Google
+        driver.get("http://www.bing.com");
+    }
+    @Test
+    public void testBingSearch() {
+        // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+        WebElement element = driver.findElement(By.name("q"));
+        // Wyczyść teskst zapisany w elemencie
+        element.clear();
+
+        // Wpisz informacje do wyszukania
+        element.sendKeys("Mistrzostwa Świata w piłce nożnej 2018");
+
+        // Prześlij formularz
+        element.submit();
+    }
+    @After
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+        driver.quit();
+    }
+}
